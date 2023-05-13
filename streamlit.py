a37a26a0215ea6d319c1cdcfdadbab76ba5d4e32
david-agenthub <132621073+david-agenthub@users.noreply.github.com>
2023-05-12 22:22:57 -0500

    streamlit.py - commit created by https://agenthub.dev/agent?run_id=c844331a-27f3-4647-aca6-42303560de3f

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,4 @@
+# The following code imports necessary libraries and modules
 import streamlit as st
 import tempfile
 import os 
@@ -11,31 +12,39 @@ import pinecone
 from templates.qa_prompt import QA_PROMPT
 from templates.condense_prompt import CONDENSE_PROMPT
 
+# The following code loads environment variables from an external file
 from dotenv import load_dotenv
 load_dotenv()
 
+# The following code defines a namespace
 pinecone_namespace ='testing-pdf-2389203901'
 
+# The following code defines the main function that runs the Streamlit app
 def main():
     # Set Streamlit app title and header
     st.title('Langchain Chat')
     st.header('PDF Mode')
 
+    # The following code defines four columns to input API keys and Pinecone indexing details
     col1, col2, col3, col4 = st.columns(4)
 
+    # Input OpenAI API Key
     with col1:
         openai_api_key = st.text_input("OpenAI API Key", type="password")
 
+    # Input Pinecone API Key
     with col2:
         pinecone_api_key = st.text_input("Pinecone API Key", type="password")
 
+    # Input Pinecone Environment
     with col3:
         pinecone_environment = st.text_input("Pinecone Environment")
 
+    # Input Pinecone Index Name
     with col4:
         pinecone_index = st.text_input("Pinecone Index Name")
 
-
+    # Upload PDF files
     uploaded_files = st.file_uploader("Upload multiple files", accept_multiple_files=True, type="pdf")
 
     if uploaded_files:
@@ -44,35 +53,48 @@ def main():
                 file_name = uploaded_file.name
                 file_content = uploaded_file.read()
                 st.write("Filename: ", file_name)
+
+                # Write the content of the PDF files to a temporary directory
                 with open(os.path.join(tmpdir, file_name), "wb") as file:
                     file.write(file_content)
+
+            # Load the PDF files from the temporary directory
             loader = DirectoryLoader(tmpdir, glob="**/*.pdf", loader_cls=PyMuPDFLoader)
             documents = loader.load()
+
+            # Split the PDF files into smaller chunks of text
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
             documents = text_splitter.split_documents(documents)
 
+            # Initialize Pinecone with API keys and environment variables
             pinecone.init(
                 api_key=pinecone_api_key,  # find at app.pinecone.io
                 environment=pinecone_environment  # next to api key in console
             )
+
+            # Initialize the OpenAI embeddings model
             embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', openai_api_key=openai_api_key)
+
+            # Add the documents to Pinecone index
             Pinecone.from_documents(documents, embeddings, index_name=pinecone_index, namespace=pinecone_namespace)
             st.success("Ingested File!")
 
+    # The following code takes user input, sets the temperature and determines the number of sources for the retrieval chain
     message = st.text_input('User Input:')
     temperature = st.slider('Temperature', 0.0, 2.0, 0.7)
     source_amount = st.slider('Sources', 1, 8, 4)
 
-
     if message:
         chat_history = []
-        embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', openai_api_key=openai_api_key)
 
+        # Initialize the OpenAI Chat model
+        embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', openai_api_key=openai_api_key)
+        model = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=temperature, openai_api_key=openai_api_key, streaming=True)
+
+        # Use Pinecone to retrieve documents relevant to user query
         pinecone.init(api_key=pinecone_api_key,environment=pinecone_environment)
         vectorstore = Pinecone.from_existing_index(index_name=pinecone_index, embedding=embeddings, text_key='text', namespace=pinecone_namespace)
-
-        model = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=temperature, openai_api_key=openai_api_key, streaming=True) # max temperature is 2 least is 0
-        retriever = vectorstore.as_retriever(search_kwargs={"k": source_amount},  qa_template=QA_PROMPT, question_generator_template=CONDENSE_PROMPT) # 9 is the max sources
+        retriever = vectorstore.as_retriever(search_kwargs={"k": source_amount},  qa_template=QA_PROMPT, question_generator_template=CONDENSE_PROMPT)
         qa = ConversationalRetrievalChain.from_llm(llm=model, retriever=retriever, return_source_documents=True)
         result = qa({"question": message, "chat_history": chat_history})
         answer = result["answer"]
@@ -101,7 +123,6 @@ def main():
             }
             parsed_documents.append(parsed_doc)
         
-
         # Display the response in the Streamlit app
         st.write('AI:')
         st.write(answer)
