60551247f10f6f1af8b05d9a094f19d59b6d4eb3
Your Name <34923485+Haste171@users.noreply.github.com>
2024-03-09 22:41:01 -0600

    Check for models, class init placement change

--- endpoints/chat.py
+++ endpoints/chat.py
@@ -1,20 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from handlers.base import BaseHandler
 from typing import Optional
 
 router = APIRouter()
-handler = BaseHandler()
 
 class ChatModel(BaseModel):
     query: str
+    model: str = "gpt-3.5-turbo"
+    temperature: float = 0.7
     chat_history: list[str] = [] # Example input: [("You are a helpful assistant.", "What is your name?")]
     namespace: Optional[str] = None 
 
 @router.post("/chat")
 async def chat( 
-    chat_model: ChatModel
+    chat_model: ChatModel,
 ):
+    available_models = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k", "gpt-4-1106-preview"]
+
+    if chat_model.model not in available_models:
+        raise HTTPException(status_code=400, detail=f"Invalid model name. Please select a valid model from the list of available models: \n{str(available_models)}")
+    
+    if chat_model.temperature < 0.0 or chat_model.temperature > 2.0:
+        raise HTTPException(status_code=400, detail="Invalid temperature value. Please select a value between 0.0 and 2.0")
+
+    handler = BaseHandler(openai_chat_model=chat_model.model, temperature=chat_model.temperature)
     response = handler.chat(
         chat_model.query, 
         chat_model.chat_history,
